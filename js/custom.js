114b674809c231b5eacacb5f8205e03e5a4440c2
Amorpheuz <mail2ypd@gmail.com>
2018-03-27 22:50:25 +0530

    Added Product details and Images to Product Page

--- js/custom.js
+++ js/custom.js
@@ -30,4 +30,45 @@ $(document).ready(function () {
     $("#topBtn").click(function () {
         $(document).scrollTop(0);
     });
+    var $carousel = $('.imgcarousel').flickity({
+        imagesLoaded: true,
+        percentPosition: false,
+    });
+
+    var $imgs = $carousel.find('.imgcarousel-cell img');
+    // get transform property
+    var docStyle = document.documentElement.style;
+    var transformProp = typeof docStyle.transform == 'string' ?
+        'transform' : 'WebkitTransform';
+    // get Flickity instance
+    var flkty = $carousel.data('flickity');
+
+    $carousel.on('scroll.flickity', function () {
+        flkty.slides.forEach(function (slide, i) {
+            var img = $imgs[i];
+            var x = (slide.target + flkty.x) * -1 / 3;
+            img.style[transformProp] = 'translateX(' + x + 'px)';
+        });
+    });
+});
+$(function () {
+    var $carousel = $('.imgcarousel').flickity({
+        imagesLoaded: true,
+        percentPosition: false,
+    });
+    var $imgs = $carousel.find('.imgcarousel-cell img');
+    // get transform property
+    var docStyle = document.documentElement.style;
+    var transformProp = typeof docStyle.transform == 'string' ?
+        'transform' : 'WebkitTransform';
+    // get Flickity instance
+    var flkty = $carousel.data('flickity');
+
+    $carousel.on('scroll.flickity', function () {
+        flkty.slides.forEach(function (slide, i) {
+            var img = $imgs[i];
+            var x = (slide.target + flkty.x) * -1 / 3;
+            img.style[transformProp] = 'translateX(' + x + 'px)';
+        });
+    });
 });
